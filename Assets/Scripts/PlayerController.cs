4093d6281046cdf3aa7baeb1c64b58ed559f1108
MadiGara <106283610+MadiGara@users.noreply.github.com>
2022-06-19 23:01:17 -0700

    UI finished and standalone build made

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 //already assigned to playerController bc we did it through add component
 {
     public float speed = 0; //can see and change in inspector bc public!
+    public TextMeshProUGUI countText; //holds reference to text UI of count - can change in inspector
+    public GameObject winTextObject;
 
     private Rigidbody rb; //private = not accessible from inspector or other scripts
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -17,6 +21,10 @@ public class PlayerController : MonoBehaviour
     {
         //set value of rb by getting reference to rigidbody component attached to the Player
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText(); //calls the count display function we have below
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue) 
@@ -27,6 +35,16 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x; //V2 types come with .x and .y members
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            //display win text if all objects collected
+            winTextObject.SetActive(true);
+        }
     }
 
     //add force to the Player's rigidbody
@@ -45,6 +63,9 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count += 1;
+
+            SetCountText();
         }
     }
 }
